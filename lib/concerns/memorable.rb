7c5ea021b829905159ec765ddc50afcc747edb49
Mario Adamo Jr <marioadamojr@gmail.com>
2019-04-14 17:31:41 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,13 @@
 module Memorable
-
-  def reset_all
-    self.all.clear
+  module InstanceMethods
   end
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
 
-  def count
-    self.all.count
+    def count
+      self.all.count
+    end
   end
 end
