7d309235229423dd3965f716796e774ec76c4ebc
Mario Adamo Jr <marioadamojr@gmail.com>
2019-04-14 18:05:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,7 @@ class Artist
   attr_accessor :name
   attr_reader :songs
   extend Memorable::ClassMethods, Findable::ClassMethods
-  include Paramable::InstanceMethods
+  include Paramable::InstanceMethods, Memorable::InstanceMethods
 
   @@artists = []
 
